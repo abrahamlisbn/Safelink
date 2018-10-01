543dba2bd8ccd4e203d1f8411229e965339f10dd
abrahamlisbn <abrahamlisbn@gmail.com>
2018-10-01 19:00:32 +0700

    Add files via upload

---
+++ marketing-is-oldest.js
@@ -0,0 +1,11 @@
+<div dir="ltr" style="text-align: left;" trbidi="on">
+<center>
+<h2 id="daplong" style="color: daplong;">
+Please Wait...</h2>
+<script src="KODE_SCRIPT" type="text/javascript"></script> <script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("http://www.computercad.xyz/2018/10/email-marketing-is-oldest-form-of.html?url=", "");
+document.write('<button id="download" class="Visit_Link" onclick="changeLink();" style="display: none;text-align:justify;margin-bottom:2px;">Visit Link</button>')
+</script></center>
+</div>
